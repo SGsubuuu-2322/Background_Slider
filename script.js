7d69fd1b2d2a62e1ac6b24528ffbdc4faffc1f93
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-21 22:13:44 +0530

    Added the script code for capturing relevant HTML elements...

---
+++ script.js
@@ -0,0 +1,4 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBTn = document.getElementById("right");
