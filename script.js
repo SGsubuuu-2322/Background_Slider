fe3423e270eb6c4de9f9307464b2fadd2dbf6ccf
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-21 22:21:20 +0530

    Added the script code for adding background image dynamically...

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftBtn = document.getElementById("left");
 const rightBTn = document.getElementById("right");
+
+let activeSlides = 0;
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlides].style.backgroundImage;
+}
